f573bdf84b228781390b0ab9d8f06e7e34b8074a
Enrico Donatoni <enrico.donatoni@studenti.unitn.it>
2018-01-23 21:33:20 +0100

    aggiunta test

--- area.js
+++ area.js
@@ -1,29 +1,36 @@
 exports.getArea = function(lati){
 	
 	var errore=0;
-
-	if(lati.length==2){
-		for(i=0; i<lati.length; i++){
-			if(typeof(lati[i]) == "undefined"){
-				errore=1;
-			}
-			else{
-				if(lati[i] != parseInt(lati[i]) || lati[i] == "" || isNaN(lati[i])){
-					errore=1;
-				}
-				else{
-					if(lati[i]<0){
+		if(typeof(lati)==="undefined"){
+			errore=1
+		}
+		else{
+			if(lati.length==2){
+				for(i=0; i<lati.length; i++){
+					if(typeof(lati[i]) === "undefined"){
 						errore=1;
 					}
+					else{
+						if(lati[i] != parseInt(lati[i])){
+							errore=1;
+						}
+						else{
 
+							if(lati[i]<0){
+								errore=1;
+							}
+							
+
+						}
+					}
 				}
+
+			}
+			else{
+				errore=1;
 			}
 		}
 
-	}
-	else{
-		errore=1;
-	}
 
 	if(errore==1){
 		return (-1);
